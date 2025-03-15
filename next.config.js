5bcc43db5bbb9743e76ac57afa79d62bad035512
samuel sabino <sabinodasilvasamuel@gmail.com>
2025-03-15 16:28:08 -0300

    chore: updating directory in config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, 
-    },
-    basePath: "/Projeto-Integrador-III", 
-    assetPrefix: "/Projeto-Integrador-III/",
-  };
-  
-  module.exports = nextConfig;
-  
+  experimental: {
+    appDir: true,
+  },
+  output: 'export', 
+  images: {
+    unoptimized: true, 
+  },
+  basePath: "/Projeto-Integrador-III", 
+  assetPrefix: "/Projeto-Integrador-III/",
+};
+
+module.exports = nextConfig;
