46d623e73505db95948e9304289c2740bc02e723
samuel sabino <sabinodasilvasamuel@gmail.com>
2025-03-15 16:03:22 -0300

    Chore: ajustando dependecias

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: 'export',
     images: {
-      unoptimized: true,
+      unoptimized: true, 
     },
-    basePath: "/Projeto-Integrador-III",
+    basePath: "/Projeto-Integrador-III", 
     assetPrefix: "/Projeto-Integrador-III/",
   };
   
