04878d192eac9b8e25e7d89121e97b2d96c75c96
samuel sabino <sabinodasilvasamuel@gmail.com>
2025-03-16 14:08:22 -0300

    update routes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  output: 'export', 
+  reactStrictMode: true,
   images: {
     unoptimized: true, 
   },
