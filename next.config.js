fc9813a9de6472efa5816b41c513a4228aac90b9
samuel sabino <sabinodasilvasamuel@gmail.com>
2025-03-25 20:59:53 -0300

    Adicionando chave privada

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   },
   basePath: "/Projeto-Integrador-III", 
   assetPrefix: "/Projeto-Integrador-III/",
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: "AIzaSyCj4nV0Kl9E36DN5VlYMEER9UwkkKMveag",
+  },
 };
 
 module.exports = nextConfig;
