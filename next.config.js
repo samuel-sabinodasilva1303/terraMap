48e0d5bb6abfdc6cf53283a56a66f3e36fed004e
samuel sabino <sabinodasilvasamuel@gmail.com>
2025-05-25 16:27:42 -0300

    update deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
-  basePath: "/terraMap", 
-  assetPrefix: "/terraMap",
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
